76e66bd59f79fba1fe3a1f22f662e48232971130
theTrueMikeBrown <theTrueMikeBrown@gmail.com>
2022-03-18 19:44:43 -0600

    Adding the reportviewer control

---
+++ TechSupport/TechSupport/Controls/OpenAssingnedIncidentsReportControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TechSupport.Controls
+{
+  public partial class OpenAssingnedIncidentsReportControl : UserControl
+  {
+    public OpenAssingnedIncidentsReportControl()
+    {
+      InitializeComponent();
+    }
+  }
+}
